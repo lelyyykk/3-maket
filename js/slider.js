b714ee911f415d8ca37651f60b44ff3222654473
Sasha Lelyk <133365539+lelyyykk@users.noreply.github.com>
2023-06-06 11:54:52 +0300

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,12 @@
-/*
+
 $(document).ready(function() {
-    $('.slider').slick();
-});
-*/
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        cssEase: 'linear'
+    })
+})
